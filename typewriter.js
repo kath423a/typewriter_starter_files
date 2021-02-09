85f32b4bcd7ab5d5de923801db1702b36903c20d
Kathrine <kath423a@stud.kea.dk>
2021-02-09 13:16:29 +0100

    loop added og virker men aner ikke hvorfor

--- typewriter.js
+++ typewriter.js
@@ -1,24 +1,34 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", init);
+
+//Opret variabler
+let text;
+let showText;
+let htmlText = document.querySelector(".typewritten");
+let i = 1;
 
 function init() {
-    console.log("init");
-    //get the text from somewhere
+  console.log("init");
 
-    //clear the HTML area 
+  //get the text from somewhere
+  text = htmlText.textContent;
 
-    // start loop
-    loop();
+  //clear the HTML area
+  htmlText.innerHTML = "";
+
+  // start loop
+  loop();
 }
 
 function loop() {
-    console.log("loop");
-    //show the N'th letter:
-    // - set textContent to substring of 0 to N
-
-    //incrementN
-
-    //wait before calling loop() again
-    if () {
-        setTimeout(loop, 500);
-    }
+  console.log("loop");
+  //show the N'th letter:
+  // - set textContent to substring of 0 to N
+  //wait before calling loop() again
+  showText = text.substring(0, i);
+  htmlText.textContent = showText;
+  if (i < text.length) {
+    i++;
+    setTimeout(loop, 500);
+  }
 }
