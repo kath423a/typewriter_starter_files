b40d800c43bb4b7212f8db9ecb78e9c484e842f4
Kathrine <kath423a@stud.kea.dk>
2021-02-09 12:56:05 +0100

    peters eksempel kopieret ind

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,24 @@
 "use strict";
+
+function init() {
+    console.log("init");
+    //get the text from somewhere
+
+    //clear the HTML area 
+
+    // start loop
+    loop();
+}
+
+function loop() {
+    console.log("loop");
+    //show the N'th letter:
+    // - set textContent to substring of 0 to N
+
+    //incrementN
+
+    //wait before calling loop() again
+    if () {
+        setTimeout(loop, 500);
+    }
+}
